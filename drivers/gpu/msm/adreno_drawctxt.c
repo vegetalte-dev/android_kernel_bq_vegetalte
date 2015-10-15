22b0123a6e0fb5609226e7742243991987957b6e
Kra1o5 <kra1o5x@gmail.com>
2015-10-15 21:35:21 +0200

    Revert "msm: kgsl: Fix fence debug message lock sequence"
    This reverts commit 6f8a6a34c651d706bd5b00f3807cbad87b5ca9da.

--- drivers/gpu/msm/adreno_drawctxt.c
+++ drivers/gpu/msm/adreno_drawctxt.c
@@ -62,12 +62,17 @@ void adreno_drawctxt_dump(struct kgsl_device *device,
 	struct adreno_context *drawctxt = ADRENO_CONTEXT(context);
 	int index, pos;
 	char buf[120];
+	int locked = 0;
 
 	kgsl_readtimestamp(device, context, KGSL_TIMESTAMP_QUEUED, &queue);
 	kgsl_readtimestamp(device, context, KGSL_TIMESTAMP_CONSUMED, &start);
 	kgsl_readtimestamp(device, context, KGSL_TIMESTAMP_RETIRED, &retire);
 
-	spin_lock(&drawctxt->lock);
+	if (!test_bit(ADRENO_CONTEXT_CMDBATCH_FLAG_FENCE_LOG,
+			&drawctxt->flags)) {
+		locked = 1;
+		spin_lock(&drawctxt->lock);
+	}
 	dev_err(device->dev,
 		"  context[%d]: queue=%d, submit=%d, start=%d, retire=%d\n",
 		context->id, queue, drawctxt->submitted_timestamp,
@@ -121,7 +126,8 @@ stats:
 	dev_err(device->dev, "  context[%d]: submit times: %s\n",
 		context->id, buf);
 
-	spin_unlock(&drawctxt->lock);
+	if (locked)
+		spin_unlock(&drawctxt->lock);
 }
 
 /**
