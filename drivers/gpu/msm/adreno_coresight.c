bff53c74f896b23ab68ca4eed161a708be3e8346
Kra1o5 <kra1o5x@gmail.com>
2015-10-21 22:41:22 +0200

    Revert "msm: kgsl: Do not get active count on coresight start/stop"
    This reverts commit 1cbec4bd07a987d76107b34744d1386ea8fd6428.

--- drivers/gpu/msm/adreno_coresight.c
+++ drivers/gpu/msm/adreno_coresight.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2013-2015, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2013-2014, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -171,16 +171,19 @@ static int _adreno_coresight_get_and_clear(struct adreno_device *adreno_dev)
 	if (coresight == NULL)
 		return -ENODEV;
 
-	kgsl_pre_hwaccess(device);
-	/*
-	 * Save the current value of each coresight register
-	 * and then clear each register
-	 */
-	for (i = 0; i < coresight->count; i++) {
-		kgsl_regread(device, coresight->registers[i].offset,
-			&coresight->registers[i].value);
-		kgsl_regwrite(device, coresight->registers[i].offset,
-			0);
+	if (!kgsl_active_count_get(device)) {
+
+		/*
+		 * Save the current value of each coresight register
+		 * and then clear each register
+		 */
+		for (i = 0; i < coresight->count; i++) {
+			kgsl_regread(device, coresight->registers[i].offset,
+				&coresight->registers[i].value);
+			kgsl_regwrite(device, coresight->registers[i].offset,
+				0);
+		}
+		kgsl_active_count_put(device);
 	}
 
 	return 0;
@@ -196,10 +199,13 @@ static int _adreno_coresight_set(struct adreno_device *adreno_dev)
 	if (coresight == NULL)
 		return -ENODEV;
 
-	BUG_ON(!kgsl_state_is_awake(device));
-	for (i = 0; i < coresight->count; i++)
-		kgsl_regwrite(device, coresight->registers[i].offset,
-			coresight->registers[i].value);
+	if (!kgsl_active_count_get(device)) {
+		for (i = 0; i < coresight->count; i++)
+			kgsl_regwrite(device, coresight->registers[i].offset,
+				coresight->registers[i].value);
+
+		kgsl_active_count_put(device);
+	}
 
 	return 0;
 }
@@ -243,11 +249,7 @@ static int adreno_coresight_enable(struct coresight_device *csdev)
 			coresight->registers[i].value =
 				coresight->registers[i].initial;
 
-		ret = kgsl_active_count_get(device);
-		if (!ret) {
-			ret = _adreno_coresight_set(adreno_dev);
-			kgsl_active_count_put(device);
-		}
+		ret = _adreno_coresight_set(adreno_dev);
 	}
 
 	mutex_unlock(&device->mutex);
